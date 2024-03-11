f715296f4e51c4c7fc870c84c9c4ac77d17fdb49
Dheeraj-Surendran <146818891+Dheeraj-Surendran@users.noreply.github.com>
2024-03-11 14:15:37 +0530

    Add files via upload

---
+++ StringSorter.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class StringSorter{
+public static void main(String[]args){
+String[] strArray= {"banana","apple","orange","grape"};
+Arrays.sort(strArray);
+System.out.println("Sorted Strings:"+ Arrays.toString(strArray));
+}}
+
