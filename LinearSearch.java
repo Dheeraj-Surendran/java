ab45f86ce99a3f578221ba286eaad8653bc6d021
Dheeraj-Surendran <146818891+Dheeraj-Surendran@users.noreply.github.com>
2024-03-18 13:19:24 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Enter the element to search for:");
+        int x = scanner.nextInt();
+        int index = linearSearch(arr, x);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+
+    public static int linearSearch(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
